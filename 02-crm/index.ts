2ac54b3cb984d26646ab8443fb98770789de2aa1
Facundo Colavini <facundo.colavini@gmail.com>
2023-11-01 12:40:16 -0300

    # Devolver un html vacio en la respuesta del server

--- 02-crm/index.ts
+++ 02-crm/index.ts
@@ -1,7 +1,16 @@
 Bun.serve({
     port: 8080, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
     fetch(req) {
-      return new Response("Hello from bum!");
+      return new Response(`
+      <html>
+       <body>
+       <h1>Hola desde bun</h1>
+       </body>
+      </html>`, {
+        headers: {
+            "Content-type": "text/html",
+            },
+      });
     },
   });
   
